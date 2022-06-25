4eb36e5450d24a58614b1dd0a4705991e3cb1ad6
eduardoaguilar96 <eduaguilar96@gmail.com>
2022-06-25 09:21:07 +0200

    maquetacion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { InputText } from 'primereact/inputtext';
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
+import { useState } from 'react';
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+ 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TRIVIAL</h1>
+        <InputText className='nombre' size={50}/>
+        <br></br>
+        <InputText className='categoria' size={50}/>
+        <br></br>
+        <Button type="button" label="Empezar juego" icon="pi pi-check"></Button>
       </header>
+      
     </div>
+  
+
   );
 }
 
