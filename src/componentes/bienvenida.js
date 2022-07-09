e8cdef7029a50df30645c09a1e36a7643d796d81
xaxi <francisco.perez@thebridgeschool.es>
2022-07-09 10:57:23 +0200

    Arreglos varios

--- src/componentes/bienvenida.js
+++ src/componentes/bienvenida.js
@@ -1,34 +1,42 @@
-import './App.css';
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import React, { useState } from 'react';
+import "./App.css";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import React from "react";
 
+import PropTypes from "prop-types";
 
-import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
-import "primereact/resources/primereact.min.css";                  //core css
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css";
 //icons
 
-
-
-
 function Bienvenida(props) {
-    return (
-        <div className="contenedorPrincipal">
-            <body className="contenedorInicio">
-                <h1>TRIVIAL</h1>
-                Nombre
-                <InputText className='nombre' size={50} />
-                <br></br>
-                Preguntas
-                <InputText className='categoria' size={50} />
-                <br></br>
-                <Button onClick={() => props.setEsPantallaPrincipal(false)} id="botoninicio" type="button" label="Empezar juego" icon="pi pi-check"></Button> <br />
-            </body>
-
-        </div>
-    );
+  return (
+    <div className="contenedorPrincipal">
+      <body className="contenedorInicio">
+        <h1>TRIVIAL</h1>
+        Nombre
+        <InputText className="nombre" size={50} />
+        <br></br>
+        Preguntas
+        <InputText className="categoria" size={50} />
+        <br></br>
+        <Button
+          onClick={() => props.setEsPantallaPrincipal(false)}
+          id="botoninicio"
+          type="button"
+          label="Empezar juego"
+          icon="pi pi-check"
+        ></Button>{" "}
+        <br />
+      </body>
+    </div>
+  );
 }
 
+Bienvenida.PropTypes = {
+  esPantallaPrincipal: PropTypes.bool,
+  setEsPantallaPrincipal: PropTypes.func,
+};
 
 export default Bienvenida;
--- src/componentes/pregunta.js
+++ src/componentes/pregunta.js
@@ -1,55 +1,51 @@
+import { Button } from "primereact/button";
+import React from "react";
 
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import React, { useState } from 'react';
-
-import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
-import "primereact/resources/primereact.min.css";                  //core css
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css";
-import "primeflex/primeflex.css"
-
+import "primeflex/primeflex.css";
 
 //icons
 
-
-
-
-function Pregunta(props) {
-
-    return (
-
-        <div className="flex align-items-center justify-content-center">
-            <div className="w-auto bg-white ">
-                <div className="text-center mb-5">
-                    <div className="text-900 text-3xl font-medium mb-3 p-4">Pregunta de Trivial que es bastante larga la verdad</div>
-                    <span className="text-600 font-medium line-height-3">Categoria</span>
-                </div>
-
-
-
-                <div class="card">
-                    <div class="card-container yellow-container">
-                        
-                        <Button id="boton-opcion" label="opciones" className="p-button-raised  block bg-yellow-500 font-bold text-center p-4 border-round mb-3 w-8 m-auto" />
-                        <Button id="boton-opcion" label="opciones" className="p-button-raised  block bg-yellow-500 font-bold text-center p-4 border-round mb-3 w-8 m-auto" />
-                        <Button id="boton-opcion" label="opciones" className="p-button-raised  block bg-yellow-500 font-bold text-center p-4 border-round mb-3 w-8 m-auto" />
-                        <Button id="boton-opcion" label="opciones" className="p-button-raised  block bg-yellow-500 font-bold text-center p-4 border-round mb-3 w-8 m-auto" />
-                        
-                    </div>
-
-                </div>
-                
-
-            </div>
-
-
-
-
+function Pregunta() {
+  return (
+    <div className="flex align-items-center justify-content-center">
+      <div className="w-auto bg-white ">
+        <div className="text-center mb-5">
+          <div className="text-900 text-3xl font-medium mb-3 p-4">
+            Pregunta de Trivial que es bastante larga la verdad
+          </div>
+          <span className="text-600 font-medium line-height-3">Categoria</span>
         </div>
 
-
-    );
+        <div className="card">
+          <div className="card-container yellow-container">
+            <Button
+              id="boton-opcion"
+              label="opciones"
+              className="p-button-raised  block bg-yellow-500 font-bold text-center p-4 border-round mb-3 w-8 m-auto"
+            />
+            <Button
+              id="boton-opcion"
+              label="opciones"
+              className="p-button-raised  block bg-yellow-500 font-bold text-center p-4 border-round mb-3 w-8 m-auto"
+            />
+            <Button
+              id="boton-opcion"
+              label="opciones"
+              className="p-button-raised  block bg-yellow-500 font-bold text-center p-4 border-round mb-3 w-8 m-auto"
+            />
+            <Button
+              id="boton-opcion"
+              label="opciones"
+              className="p-button-raised  block bg-yellow-500 font-bold text-center p-4 border-round mb-3 w-8 m-auto"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
 
 export default Pregunta;
