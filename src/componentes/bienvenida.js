53df1d7c38b065dcc3cd66601d1ea075344e0174
Ulyssbat <stewartbatallasminda@gmail.com>
2022-07-12 20:08:17 +0200

    añadimos fetch de categorias y lo mostramos en pantalla

--- src/componentes/bienvenida.js
+++ src/componentes/bienvenida.js
@@ -1,25 +1,28 @@
 import "./App.css";
+import React, { useState, useEffect } from 'react';
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
-import React from "react";
-
-import PropTypes from "prop-types";
-
 import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css";
+import PropTypes from 'prop-types';
+import SelectCategoria from "./categoria";
 //icons
 
 function Bienvenida(props) {
+  const [categoria, setCategoria] = useState();
+
   return (
     <div className="contenedorPrincipal">
       <body className="contenedorInicio">
         <h1>TRIVIAL</h1>
-        Nombre
+        <h3>Nombre</h3>
         <InputText className="nombre" size={50} />
         <br></br>
-        Preguntas
-        <InputText className="categoria" size={50} />
+        <h3>Elige la categoria</h3>
+        <label className="categoria" />
+        <SelectCategoria setCategoria={setCategoria} />
+        {categoria}
         <br></br>
         <Button
           onClick={() => props.setEsPantallaPrincipal(false)}
