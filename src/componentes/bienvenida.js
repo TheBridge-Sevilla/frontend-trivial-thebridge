309625474dced0582edbd218d3da2396ad80fc2d
xaxi <francisco.perez@thebridgeschool.es>
2022-07-14 20:53:51 +0200

    Arreglo rapido

--- src/componentes/bienvenida.js
+++ src/componentes/bienvenida.js
@@ -1,21 +1,21 @@
 import "./App.css";
-import React, { useState, useEffect } from 'react';
+import React, { useState } from "react";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css";
-import PropTypes from 'prop-types';
 import SelectCategoria from "./categoria";
 import { useTranslation } from "react-i18next";
 
 function Bienvenida(props) {
   const { t } = useTranslation();
+  const { categoria, setCategoria } = useState("");
 
   return (
     <div className="contenedorPrincipal">
       <body className="contenedorInicio">
-        <h1>{t('trivial')}</h1>
+        <h1>{t("trivial")}</h1>
         <h3>Nombre</h3>
         <InputText className="nombre" size={50} />
         <br></br>
@@ -32,8 +32,8 @@ function Bienvenida(props) {
           icon="pi pi-check"
         ></Button>{" "}
         <br />
-      </body >
-    </div >
+      </body>
+    </div>
   );
 }
 
