473c43c1238bb8c4d981472c5418447b792c6cd5
idavid80 <dominguezbueno.david@gmail.com>
2022-07-19 12:48:57 +0200

    maqueta resultado y boton select sin acabar

--- src/componentes/resultado.js
+++ src/componentes/resultado.js
@@ -1,41 +1,42 @@
-import "./App.css";
-//import { InputText } from "primereact/inputtext";
-import { Button } from "primereact/button";
 import React from "react";
 import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import Boton from "./boton-opciones";
+import "./Ranking.css";
 
-function FinPartida(props) {
+function FinPartida() {
+
+
   return (
-    <div className="card">
-      <div className="card-container purple-container m-auto bg-cyan-500">
-        <div className="bg-cyan-500 border-round-top p-8 font-bold text-gray-900">
-          <h1 className="m-auto">Resultado</h1>
-        </div>
-        <div className="bg-yellow-500 border-round-top-xl p-8 font-bold text-gray-900">
-          <label className="nombre" />
-          <br></br>
-          <label className="categoria" />
-          <br></br>
-          <div className="w-13rem h-12 mb-3">
-            <ol className="block appearance-none w-full h-full p-3 border-round" id="ranking">
-              <li>Programación</li>
-              <li>Musica</li>
-              <li>Deporte</li>
-            </ol>
+    <div className="flex align-items-center justify-content-center">
+      <div className="w-full bg-cyan-600" id="fin-partida">
+        <div className="w-auto bg-bluegray-100 text-center p-5">
+          <div className="text-center text-3xl font-medium p-4" id="result-1">
+            Fin de Partida
           </div>
-          <div className="border-round-top-xl p-2 font-bold text-gray-900">
-          <Button
-            onClick={() => props.setEsPantallaPrincipal(false)}
-            id="botoninicio"
-            type="button"
-            label="Volver a jugar"
-            icon="pi pi-check"
-          ></Button>{" "}
+          <div className="text-center text-3xl font-medium p-4" id="result-1">
+            Tú puntuación es 5/5
+          </div>
+        </div>
+        <div className="w-full bg-blue-300 text-center p-4"><h3>Ranking</h3>
+
+          <div className="w-full flex justify-content-center">
+
+            <table id="tabla">
+              <tr><th>Posición</th><th>Jugador</th><th>Puntos</th><th>Categoria</th></tr>
+              <tr><td># 100</td><td>Jugador 123456123131345</td><td>5/10</td><td>Programación</td></tr>
+              <tr><td># 1</td><td>Jugador 1</td><td>5/5</td><td>Música</td></tr>
+              <tr><td># 2</td><td>Jugador 2</td><td>4/5</td><td>Deporte</td></tr>
+            </table>
+          </div>
+        </div>
+
+        <div className="card-container yellow-container mb-5 p-5">
+          <Boton disposicion="correcta" />
         </div>
       </div>
-    </div>
     </div>
   );
 }
