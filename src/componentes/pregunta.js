151c79ecbcad37ffcdf204e73b91ed253cae81f0
xaxi <francisco.perez@thebridgeschool.es>
2022-07-23 13:06:15 +0200

    Pregunta funciona

--- src/componentes/pregunta.js
+++ src/componentes/pregunta.js
@@ -5,22 +5,22 @@ import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 import Boton from "./boton-opciones";
 import Reloj from "./tiempo";
-//import PrimeraPregunta from "./PrimeraPregunta";
 import "./pregunta.css";
 
 function Pregunta(props) {
+
   return (
     <div className="flex align-items-center justify-content-center">
       <div className="w-auto bg-cyan-600" id="pregunta">
         <div className="w-auto bg-blue-300 text-center mb-5">
           <div className="text-900 text-3xl font-medium mb-3 p-4">
-            <h2>Pregunta de Trivial que es bastante larga la verdad</h2>
+            <h2>{props.pregunta.Pregunta}</h2>
           </div>
           <span className="text-600 font-medium line-height-3">Categoria</span>
         </div>
 
         <div className="card">
-          <Reloj />
+        <Reloj />
           <div className="card-container yellow-container">
             <Boton disposicion="correcta" />
             <Boton disposicion="incorrecta" />
--- src/componentes/preguntas.js
+++ src/componentes/preguntas.js
@@ -1,23 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import Pregunta from './pregunta'
+import React, { useState, useEffect } from "react";
+import Pregunta from "./pregunta";
 
 function Preguntas(props) {
-
-    const url = 'http://localhost:3050/preguntas/categoria';
-    //const [preguntas, setPreguntas] = useState([]);
-
-    useEffect(() => {
-        const formCategoria = new FormData();
-        formCategoria.append('categoria', props.categoria);
-        console.log(props)
-        fetch(url , { method: 'POST', body: formCategoria}).then(res => res.json()).then(json =>  console.log(json))
-    });
-    return (
-        <p>
-            {
-                //preguntas.map(pregunta => <Pregunta key={pregunta} >{pregunta}</Pregunta>)
-            }
-        </p >)
+  const url = "http://localhost:3050/preguntas/categoria";
+  const [preguntas, setPreguntas] = useState([]);
+  console.log(props.categoria);
+  useEffect(() => {
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ categoria: props.categoria }),
+    })
+      .then((res) => res.json())
+      .then((json) => setPreguntas(json));
+  });
+  return (
+    <p>
+      {preguntas.map((pregunta) => (
+        <Pregunta key={pregunta} pregunta = {pregunta}/>
+      ))}
+    </p>
+  );
 }
 
-export default Preguntas
+export default Preguntas;
