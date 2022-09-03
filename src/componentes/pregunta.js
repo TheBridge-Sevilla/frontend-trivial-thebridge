7ba01cd4e4e7e2d68aafa9858d4cecdfffc592d4
xaxi <francisco.perez@thebridgeschool.es>
2022-09-03 11:24:09 +0200

    Titulo añadido

--- src/componentes/pregunta.js
+++ src/componentes/pregunta.js
@@ -9,6 +9,7 @@ import "./pregunta.css";
 import { useTranslation } from "react-i18next";
 
 function Pregunta(props) {
+  console.log(props)
   const { i18n } = useTranslation();
   
   const handleIndicePreguntas = () => {
@@ -24,7 +25,7 @@ function Pregunta(props) {
       <div className="w-auto bg-cyan-600" id="pregunta">
         <div className="w-auto bg-blue-300 text-center mb-5">
           <div className="text-900 text-3xl font-medium mb-3 p-4">
-            <h2>{props.pregunta.Pregunta}</h2>
+            <h2>{props.pregunta.pregunta[i18n.language]}</h2>
           </div>
           <span className="text-600 font-medium line-height-3">Categoria</span>
         </div>
