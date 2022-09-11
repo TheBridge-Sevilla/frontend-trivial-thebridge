219ba292175e9ba408a4f03d4f4fe3d0c19235c5
Ulyssbat <stewartbatallasminda@gmail.com>
2022-09-11 18:59:34 +0200

    arreglado pequenyo error al editar

--- src/componentes/pregunta.js
+++ src/componentes/pregunta.js
@@ -18,7 +18,7 @@ function Pregunta(props) {
 
   useEffect(() => {
     const pasarPregunta = setTimeout(() => {
-      props.setIndicePregunta(props.indicePregunta);
+      props.setIndicePregunta(props.indicePregunta + 1);
     }, 20000);
     return () => clearTimeout(pasarPregunta);
   }, [props.indicePregunta]);
