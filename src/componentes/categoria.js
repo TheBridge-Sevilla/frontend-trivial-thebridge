0f59871fd80acfa4c3aff9328cccbbb03f58dc5d
xaxi <francisco.perez@thebridgeschool.es>
2022-09-27 20:30:57 +0200

    No termina de funcionar, falta ID-NOMBRE categorias

--- src/componentes/categoria.js
+++ src/componentes/categoria.js
@@ -1,22 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { Dropdown } from 'primereact/dropdown';
+import React, { useState, useEffect } from "react";
+import { Dropdown } from "primereact/dropdown";
+import { useTranslation } from "react-i18next";
 
 function SelectCategoria(props) {
+  const { i18n } = useTranslation();
+  const url = "http://localhost:3050/categorias";
+  const [categorias, setCategorias] = useState([]);
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState();
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) =>
+        setCategorias(json.map((categoria) => categoria._id))
+      );
+  }, [i18n.language]);
+  const handleCategorias = (e) => {
+    setCategoriaSeleccionada(e.value), props.setCategoria(e.target.value);
+  };
 
-    const url = 'http://localhost:3050/categorias';
-    const [categorias, setCategorias] = useState([]);
-    const [categoriaSeleccionada, setCategoriaSeleccionada] = useState();
-    useEffect(() => {
-        console.log('useffect categoria')
-        fetch(url).then(res => res.json()).then(json => setCategorias(json.map(categoria => categoria.nombre)))
-        console.log(categorias)
-    }, []);
-    const handleCategorias = (e) => { setCategoriaSeleccionada(e.value), props.setCategoria(e.target.value) }
-
-    return (
-        <Dropdown className='w-13rem'
-            value={categoriaSeleccionada} options={categorias}
-            onChange={(e) => handleCategorias(e)} placeholder="Categorias" />)
+  return (
+    <Dropdown
+      className="w-13rem"
+      value={categoriaSeleccionada}
+      options={categorias}
+      onChange={(e) => handleCategorias(e)}
+      placeholder="Categorias"
+    />
+  );
 }
 
-export default SelectCategoria
+export default SelectCategoria;
