4cb50ac78e4e231398c637385668ac8d1103598a
xaxi <francisco.perez@thebridgeschool.es>
2022-09-29 23:45:17 +0200

    funcionando

--- src/componentes/pregunta.js
+++ src/componentes/pregunta.js
@@ -25,7 +25,7 @@ function Pregunta(props) {
     <div className="bg-cyan-500 p-4 w-full h-full p-4">
       <div className="w-auto h-auto bg-blue-300 text-center text-900 font-auto p-4">
       <h2 className="text-lg md:text-2xl lg:text-4xl">{props.pregunta.pregunta[i18n.language]}</h2>
-        <span className="text-purple-800 text-lg line-height-3">{props.categoria}</span>
+        <span className="text-purple-800 text-lg line-height-3">{props.categoria.nombre[i18n.language]}</span>
       </div>
       <div className="card w-auto h-auto">
         <Reloj />
