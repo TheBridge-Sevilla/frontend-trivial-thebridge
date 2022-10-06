892c3d9c7599f9fb8c4a8b8a05be0b9eabba04ee
Ulyssbat <stewartbatallasminda@gmail.com>
2022-10-06 21:18:20 +0200

    arreglado coloreado del boton al pulsar respuesta correcta o incorrecta

--- src/componentes/boton-opciones.js
+++ src/componentes/boton-opciones.js
@@ -3,17 +3,17 @@ import { Button } from "primereact/button";
 
 
 export default function Boton(props) {
-  const [colorBoton, setColorBoton] = useState("bluegray");
+  const [colorBoton, setColorBoton] = useState("bluegray-200");
 
   const handleIndicePreguntas = () => {
 
     if (!props.botonSelecionado) {
       if (props.opcion == props.respuesta) {
-        setColorBoton("green");
+        setColorBoton("green-500");
         props.setPuntuacion(props.puntuacion + 1)
 
       } else {
-        setColorBoton("red");
+        setColorBoton("red-500");
       }
       props.setBotonSelecionado(true);
 
@@ -32,7 +32,7 @@ export default function Boton(props) {
       id="boton-opcion"
       label={props.opcion}
       className={
-        `font-bold text-center p-4 border-round mb-3 w-8 m-auto p-button-raised block bg-${colorBoton}-200
+        `font-bold text-center p-4 border-round mb-3 w-8 m-auto p-button-raised block bg-${colorBoton}
         transition-colors transition-duration-500 hover:border-300`
       }
     />
