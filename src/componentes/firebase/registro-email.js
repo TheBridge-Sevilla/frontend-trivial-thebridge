f93b37a430fac8abe4bb2cbc36d7ac486a3cac8c
P-Kempf <paulonwein@gmail.com>
2022-10-11 20:32:55 +0200

    cambio de alerta a mensaje en linea

--- src/componentes/firebase/registro-email.js
+++ src/componentes/firebase/registro-email.js
@@ -1,4 +1,4 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import Usuario from "./usuario"
 import { Dialog } from 'primereact/dialog';
@@ -6,12 +6,27 @@ import { Button } from 'primereact/button';
 import { useContextoUsuario } from "../contexto/contextoUsuario";
 import { auth } from "./firebase"
 import { onAuthStateChanged } from "firebase/auth";
+import { Messages } from 'primereact/messages';
 
 
 const RegistroConEmail = () => {
   //Display responsive es un estado necesario para el Dialog, elemento de PrimeReact
-  const { usuario, setUsuario, setDisabledInputName, displayResponsive, setDisplayResponsive } = useContextoUsuario();
+  const { usuario, setUsuario, setDisabledInputName, displayResponsive, setDisplayResponsive,mensaje,setMensaje,tipo,setTipo  } = useContextoUsuario();
+  const messages = useRef();
   const { t } = useTranslation();
+
+  const mostrarError = (tipo, mensaje) => {
+    messages.current.show({ severity: `${tipo}`, detail: `${mensaje}`, life: 3000 });
+  }
+  useEffect(() => {
+    if (mensaje) mostrarError(tipo, mensaje)
+
+    return (() => {
+      setMensaje()
+      setTipo()
+    })
+
+  }, [mensaje])
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (respuesta) => {
@@ -28,6 +43,7 @@ const RegistroConEmail = () => {
       {usuario ? <></> : <Button className="mx-1" label={t("crear-cuenta")} icon="pi pi-user" onClick={() => setDisplayResponsive(true)} />}
 
       <Dialog visible={displayResponsive} onHide={() => setDisplayResponsive(!displayResponsive)} breakpoints={{ '960px': '75vw' }} style={{ width: '50vw' }} >
+        <Messages ref={messages} />
         <Usuario />
       </Dialog>
 
