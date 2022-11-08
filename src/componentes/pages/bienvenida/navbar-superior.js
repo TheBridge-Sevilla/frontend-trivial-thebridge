810d5daf7fae5bf9172e2ddee606b6cf46dddfb5
xaxi <francisco.perez@thebridgeschool.es>
2022-11-08 20:58:09 +0100

    Traducciones en navbar superior arregladas

--- src/componentes/pages/bienvenida/navbar-superior.js
+++ src/componentes/pages/bienvenida/navbar-superior.js
@@ -1,59 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import { Menubar } from 'primereact/menubar';
-import { useContextoUsuario } from '../../contexto/contextoUsuario';
-import { useSignOut } from '../../customHooks/hook-cerrar-sesion';
-import CambiarIdioma from '../../acciones/cambiar-idioma';
+import React, { useEffect, useState } from "react";
+import { Menubar } from "primereact/menubar";
+import { useContextoUsuario } from "../../contexto/contextoUsuario";
+import { useSignOut } from "../../customHooks/hook-cerrar-sesion";
+import CambiarIdioma from "../../acciones/cambiar-idioma";
 import { useTranslation } from "react-i18next";
 
-
 export const HeaderBar = (props) => {
-    const { t } = useTranslation();
-    const { setVisibleTop, currentUser } = useContextoUsuario();
-    const { cerrarSesion } = useSignOut()
+  const { t, i18n } = useTranslation();
+  const { setVisibleTop, currentUser } = useContextoUsuario();
+  const { cerrarSesion } = useSignOut();
 
-    const itemsOptions = [
+  const [items, setItems] = useState([
+    {
+      label: t("iniciar-sesion"),
+      icon: "pi pi-user-plus",
+      command: () => {
+        setVisibleTop(true);
+      },
+    },
+  ]);
+
+  useEffect(() => {
+    if (currentUser) {
+      setItems([
         {
-            label: t("iniciar-sesion"),
-            icon: 'pi pi-user-plus',
-            command: () => { setVisibleTop(true) },
+          label: "User",
+          icon: "pi pi-user",
         },
         {
-            label: 'User',
-            icon: 'pi pi-user',
+          label: "Usuario",
+          icon: "pi pi-sign-out",
+          command: () => {
+            cerrarSesion();
+          },
         },
+      ]);
+    } else if (props.disabledLogIn) {
+      setItems([
         {
-            label: 'Usuario',
-            icon: 'pi pi-sign-out',
-            command: () => { cerrarSesion() }
+          label: "Usuario invitado",
+          icon: "pi pi-user",
+          disabled: "true",
         },
+      ]);
+    }
+    return () => {
+      setItems([
         {
-            label: 'Usuario invitado',
-            icon: 'pi pi-user',
-            disabled: 'true'
-        }]
+          label: t("iniciar-sesion"),
+          icon: "pi pi-user-plus",
+          command: () => {
+            setVisibleTop(true);
+          },
+        },
+      ]);
+    };
+  }, [currentUser, props.disabledLogIn, i18n.language]);
 
-    const [items, setItems] = useState(
-        [itemsOptions[0]]
-    )
-
-    useEffect(() => {
-        if (currentUser) {
-            setItems([
-                itemsOptions[1],
-                itemsOptions[2]
-            ])
-        }
-        else if (props.disabledLogIn) {
-            setItems([itemsOptions[3]])
-        }
-        return () => {
-            setItems([itemsOptions[0]])
-        }
-    }, [currentUser, props.disabledLogIn])
-
-    return (
-        <div className="card">
-            <Menubar model={items} end={CambiarIdioma()} />
-        </div>
-    );
-}
+  return (
+    <div className="card">
+      <Menubar model={items} end={CambiarIdioma()} />
+    </div>
+  );
+};
