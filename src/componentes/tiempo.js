44163c024d42e54e367d9a18c3cff9de87c606c2
franalvarez88 <104025411+franalvarez88@users.noreply.github.com>
2022-11-08 21:09:47 +0100

    limpiando

--- src/componentes/tiempo.js
+++ src/componentes/tiempo.js
@@ -60,9 +60,9 @@ export default function Reloj(props) {
 
     return (
 
-        <div className="flex align-items-center justify-content-center card-container blue-container m-0">
+        <div className="flex align-items-center justify-content-center card-container blue-container p-0">
             <div className="scalein animation-duration-500 animation-iteration-1 flex align-items-center justify-content-center
-            font-bold lg:text-lg surface-300 border-primary border-3 text-blue-600 border-round-xl lg:border-circle m-2 px-5 py-3 lg:w-11rem -mt-5 md:-mt-5 lg:m-0  shadow-8" id="crono">{tiempo} </div>
+            font-bold lg:text-lg surface-300 border-primary border-3 text-blue-600 border-round-xl lg:border-circle m-2 px-5 py-3 lg:w-11rem lg:h-4rem -mt-5 md:-mt-5 lg:m-0  shadow-8" id="crono">{tiempo} </div>
         </div>
     );
 }
