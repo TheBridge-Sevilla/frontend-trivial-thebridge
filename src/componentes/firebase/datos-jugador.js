7df4b2e2b5311ff953ebd0baf6c27203cf7110e5
P-Kempf <paulonwein@gmail.com>
2022-11-17 19:17:36 +0100

    cambios visuales en el  menu de jugador

--- src/componentes/firebase/datos-jugador.js
+++ src/componentes/firebase/datos-jugador.js
@@ -8,6 +8,8 @@ import { InputText } from "primereact/inputtext";
 import { useTranslation } from "react-i18next";
 import { uploadBytes, ref, getDownloadURL } from "firebase/storage"
 import UsuarioClasificacion from '../acciones/usuarioClasificacion'
+import 'primeicons/primeicons.css';
+import { Badge } from 'primereact/badge';
 
 
 
@@ -25,12 +27,12 @@ function DatosJugador() {
     const [nombre, setNombre] = useState()
     const email = currentUser.email
     const [imagenPerfil, setImagenPerfil] = useState("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png")
+    const iconoFoto = <img src="https://cdn-icons-png.flaticon.com/512/32/32220.png" />
 
     //actualizar foto de perfil
 
     async function upload(file, currentUser) {
         const fileRef = ref(storage, currentUser.uid + '.png');
-
 
 
         await uploadBytes(fileRef, file);
@@ -113,23 +115,29 @@ function DatosJugador() {
     }
 
     return (
-        <div>
+        <div className="form flex flex-column  ">
+            <h2 className='flex align-items-center justify-content-center text-4xl  mb-4 text-primary'>Menú</h2>
             <label style={{ cursor: 'pointer' }} htmlFor="file-input">
-                <Avatar image={imagenPerfil} referrerPolicy="no-referrer" className="mr-2 shadow-5" size="xlarge" shape="circle" />
+                <div className="relative">
+                    <Avatar image={imagenPerfil} referrerPolicy="no-referrer" className="p-overlay-badge block m-auto mb-4 shadow-5 flex align-items-center justify-content-center  " size="xlarge" shape="circle"  >
+                        <Badge value={iconoFoto} className="shadow-5"  />
+                    </Avatar>
+                </div>
             </label>
             <input id="file-input" type="file" onChange={handleChange} hidden={true} />
             {loading ? <Button label={t("subir")} onClick={handleClick}></Button> : <></>}
-            <p>{t("nombre-jugador")} :{nombre}</p>
-            <p>{t("email")} : {email}</p>
+            <p className='flex align-items-center justify-content-center  m-2 bold'>{nombre}</p>
+            <p className='flex align-items-center justify-content-center  m-3 bold'> {email}</p>
             <UsuarioClasificacion />
-            <Button label={t("cambiar-nombre")} onClick={() => setCambioNombre(!cambioNombre)}></Button>
-            {cambioNombre ? <div><InputText placeholder={t("nombre")} type="name" ref={nombreRef} />
-                <Button type="submit" label={t("actualizar-nombre")} onClick={onSubmitNombre}></Button> </div> : ""}
+            <Button className='flex align-items-center justify-content-center  m-auto my-3 font-bold w-9' label={t("cambiar-nombre")} onClick={() => setCambioNombre(!cambioNombre)}></Button>
+            {cambioNombre ? <div className="flex justify-content-center flex-wrap" ><InputText className='flex align-items-center justify-content-center  m-auto font-bold' placeholder={t("nombre")} type="name" ref={nombreRef} />
+                <Button className='flex align-items-center justify-content-center  my-2 font-bold p-button-outlined ' type="submit" label={t("actualizar-nombre")} onClick={onSubmitNombre}></Button> </div> : ""}
 
-            <Button label={t("cambiar-contraseña")} onClick={() => setCambioContraseña(!CambioContraseña)}></Button>
-            {CambioContraseña ? <div><InputText placeholder={t("contraseña")} type="password" ref={contraseñaRef} />
-                <Button type="submit" label={t("actualizar-contraseña")} onClick={onSubmitContraseña}></Button> </div> : ""}
+            <Button className='flex align-items-center justify-content-center  m-auto mb-2 font-bold w-9' label={t("cambiar-contraseña")} onClick={() => setCambioContraseña(!CambioContraseña)}></Button>
+            {CambioContraseña ? <div className='flex justify-content-center flex-wrap ' ><InputText className='flex align-items-center justify-content-center  m-auto font-bold' placeholder={t("contraseña")} type="password" ref={contraseñaRef} />
+                <Button className='flex align-items-center justify-content-center  my-2 font-bold p-button-outlined  ' type="submit" label={t("actualizar-contraseña")} onClick={onSubmitContraseña}></Button> </div> : ""}
         </div>
+
     )
 
 
