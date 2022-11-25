68ada8c39f971bf4b4ef5645da494efcef3b9452
swbatallas <stewartbatallasminda@gmail.com>
2022-11-25 19:02:14 +0100

    iniciacion del footer

--- src/componentes/pages/bienvenida/creditos.js
+++ src/componentes/pages/bienvenida/creditos.js
@@ -6,16 +6,37 @@ export default function Creditos() {
     <div className='wrapper'>
       <div className='static-txt'>Hecho por</div>
       <ul className='dynamic-txts'>
-        <a href='https://github.com/eduardoaguilar96'><li><span>eduardoaguilar96</span></li></a>
-        <a href='https://github.com/franalvarez88'><li><span>franalvarez88 </span></li></a>
-        <a href='https://github.com/swbatallas'><li><span>swbatallas </span></li></a>
-        <a href='https://github.com/frapercanFS'><li><span>frapercan </span></li></a>
-        <a href='https://github.com/idavid80'><li><span>idavid80</span></li></a>
-        <a href='https://github.com/P-Kempf'><li><span>P-Kempf</span></li></a>
-
-
+        <a href='https://github.com/eduardoaguilar96'>
+          <li>
+            <span>eduardoaguilar96</span>
+          </li>
+        </a>
+        <a href='https://github.com/franalvarez88'>
+          <li>
+            <span>franalvarez88 </span>
+          </li>
+        </a>
+        <a href='https://github.com/swbatallas'>
+          <li>
+            <span>swbatallas </span>
+          </li>
+        </a>
+        <a href='https://github.com/frapercanFS'>
+          <li>
+            <span>frapercan </span>
+          </li>
+        </a>
+        <a href='https://github.com/idavid80'>
+          <li>
+            <span>idavid80</span>
+          </li>
+        </a>
+        <a href='https://github.com/P-Kempf'>
+          <li>
+            <span>P-Kempf</span>
+          </li>
+        </a>
       </ul>
-
     </div>
   )
 }
--- src/componentes/pages/bienvenida/navbar-superior.js
+++ src/componentes/pages/bienvenida/navbar-superior.js
@@ -9,30 +9,45 @@ import { Image } from 'primereact/image';
 
 export const HeaderBar = (props) => {
 
-    const { currentUser, usuario,setVisibleLeft } = useContextoUsuario();
+    const { currentUser, usuario, setVisibleLeft } = useContextoUsuario();
     const { cerrarSesion } = useSignOut()
     const { Item } = useItem()
-
     const [item, setItem] = useState([Item()])
+    const itemGithub = [`Made with love ${<i className="pi pi-check"></i>}`, 'pi pi-github', () => {window.open('http://www.google.com')}]
 
     useEffect(() => {
         if (currentUser) {
             setItem([
                 Item(usuario, 'pi pi-user', () => setVisibleLeft(true)),
-                Item('cerrar-sesion', 'pi pi-sign-out', () => { cerrarSesion() })
+                Item('cerrar-sesion', 'pi pi-sign-out', () => { cerrarSesion() }),
+                Item(itemGithub[0], itemGithub[1], itemGithub[2])
             ])
         }
         else if (props.disabledLogIn) {
-            setItem([Item('usuario-invitado', 'pi pi-user',undefined,true)])
+            setItem([
+                Item('usuario-invitado', 'pi pi-user', undefined, true),
+                Item(itemGithub[0], itemGithub[1], itemGithub[2])
+
+            ])
         }
         return () => {
-            setItem([Item()])
+            setItem(
+                [
+                    Item(),
+                    Item(itemGithub[0], itemGithub[1], itemGithub[2])
+                ]
+            )
         }
     }, [currentUser, props.disabledLogIn, i18next.language])
 
     return (
         <div className="card ">
-            <Menubar model={item} start={<Image width="50" height="50" src='media/logo-fragen.png'  id="rotar" className="" />} end={CambiarIdioma()} className="surface-300 border-noround border-none" />
+            <Menubar
+                model={item}
+                start={<Image width="50" height="50" src='media/logo-fragen.png' id="rotar" />}
+                end={CambiarIdioma()}
+                className="surface-300 border-noround border-none"
+            />
         </div>
     );
 }
