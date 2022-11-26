1df0b86d60ca159a4d49ed5310619f4dc8cab3ad
[eduardoaguilar96] <[eduaguilar96@gmail.com]>
2022-11-26 12:21:08 +0100

    cambios background

--- src/componentes/pages/findepartida/fin-partida.js
+++ src/componentes/pages/findepartida/fin-partida.js
@@ -7,32 +7,33 @@ import { Button } from "primereact/button";
 import Clasificacion from "../../acciones/clasificacion";
 
 function FinPartida(props) {
+  const resultado = props.puntuacion * 100 / props.indicePregunta
+  
 
   return (
-    <div className="bg-cyan-500 h-screen w-screen">
-      <div className="w-full bg-cyan-500 p-4">
-        <div className="w-auto bg-bluegray-100 text-center p-4">
-          <div className="card-container text-center text-3xl font-medium">
-            Fin de Partida
-          </div>
-          <div className="card-container text-center text-3xl font-medium">
-            Tú puntuación es: {props.partida.puntuacion}
-          </div>
+    <div className="w-auto2 h-screen" style={{ backgroundImage: `url("media/fondo2.jpg")` }}>
+      <br></br><br></br>
+      <div className=" bg-bluegray-100 text-center p-4 shadow-4">
+      
+
+        <div className="card-container text-center text-3xl font-medium  ">
+          {props.puntuacion + "/" + props.indicePregunta} ({resultado} %) preguntas acertadas
         </div>
-        <div className="w-full bg-blue-300 text-center">
-          <h3>Ranking</h3>
-          <Clasificacion categoria={props.partida.categoriaID} />
-        </div>
-        <div className="card-container yellow-container p-5">
-          <Button
-            className="p-button-raised block bg-yellow-500 font-bold text-center p-4 border-round w-auto m-auto"
-            onClick={() => props.setEsPantallaPrincipal(true)}
-          >
-            Volver a jugar
-          </Button>
-        </div>
+        <br></br>
+        <Button className="p-button-raised block shadow-8 bg-yellow-500 font-bold text-center p-4 border-round w-auto m-auto"
+          onClick={() => props.setEsPantallaPrincipal(true)}>
+          Jugar de nuevo
+        </Button>
       </div>
-    </div>
+      <br></br> <br></br>
+      <div className="w-full-1 p-4 shadow-8">
+        
+        <div className="w-full-2 shadow-6  text-center"><h3 id="clasificacion">CLASIFICACIÓN</h3>
+          <Clasificacion />
+        </div>
+
+      </div>
+    </div >
   );
 }
 
